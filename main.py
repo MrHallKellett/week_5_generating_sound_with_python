12b52a21196f94304ffac63b18ffb073c834e753
MrHallKellett <142761867+MrHallKellett@users.noreply.github.com>
2023-09-24 16:27:38 +0800

    upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import array
+import math
+import time
+import pyaudio
+
+VOLUME = 0.5
+p = pyaudio.PyAudio()
+
+
+def generate_tone(duration, frequency, sample_rate):
+
+    num_samples = int(sample_rate * duration)
+    samples = []
+
+    for k in range(0, num_samples):
+        this_sample = VOLUME * math.sin(
+            2 * math.pi * k * frequency / sample_rate)
+        samples.append(this_sample)
+
+    output_bytes = array.array("f", samples).tobytes()
+
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=sample_rate,
+                    output=True)
+
+    start_time = time.time()
+    stream.write(output_bytes)
+    print("Played sound for {:.2f} seconds".format(time.time() - start_time))
+
+    stream.stop_stream()
+    stream.close()
+
+
+if __name__ == "__main__":
+    
+    ## your answers here
+    p.terminate()
